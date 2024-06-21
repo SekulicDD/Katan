58d913bd644e7d4af19564cbd1f25475f4d4aed4
David Sekulic <sekulicdd@gmail.com>
2024-06-21 17:06:51 +0200

    define main component & routes

--- angular-ui/src/app/app.routes.ts
+++ angular-ui/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LobbyComponent } from './lobby/lobby.component';
+import { RoomComponent } from './room/room.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'lobby',
+    component: LobbyComponent,
+  },
+  {
+    path: 'room/:id',
+    component: RoomComponent,
+  },
+];
